4fdfd0534772dad9a1eba7be99cfa5645035f6fe
liu_yan <liu_yan@corp.netease.com>
2015-08-21 11:39:09 +0800

    upload a file

--- commonFun.js
+++ commonFun.js
@@ -185,10 +185,10 @@ define(function(require, exports, module) {
                 time = startTime.split(' ')[0];
             }
             return time;
-        };
+        },
         
         /* 从右往左每三位数字加个逗号 */   
-        this.separateNum : function(num) {
+        separateNum : function(num) {
             var ret;
             ret = [];
             while (num) {
@@ -198,7 +198,28 @@ define(function(require, exports, module) {
                 });
             }
             return ret.join(",");
-        };
+        },
+
+        /* 重写toFixed方法 */
+        Number.prototype.toFixed : function(len)  
+        {  
+            var tempNum = 0;  
+            var s,temp;  
+            var s1 = this + "";  
+            var start = s1.indexOf(".");  
+              
+            //截取小数点后,0之后的数字，判断是否大于5，如果大于5这入为1  
+
+           if(s1.substr(start+len+1,1)>=5)  
+            tempNum=1;  
+
+            //计算10的len次方,把原数字扩大它要保留的小数位数的倍数  
+          var temp = Math.pow(10,len);  
+            //求最接近this * temp的最小数字  
+            //floor() 方法执行的是向下取整计算，它返回的是小于或等于函数参数，并且与之最接近的整数  
+            s = Math.floor(this * temp) + tempNum;  
+            return s/temp;  
+        }
         
     }
 });
